eb4b4277e037d69cc127741836596eee8809a45b
Alexander Batalov <alexbtlv@gmail.com>
2014-07-19 21:28:22 +0600

    synched app to current version from teamtreehouse.com

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617065816) do
+ActiveRecord::Schema.define(version: 20140206195317) do
 
   create_table "todo_items", force: true do |t|
     t.integer  "todo_list_id"
