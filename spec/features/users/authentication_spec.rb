2caf1e2c1e988ec21e80814e2ec8390a5b182f5a
Alexander Batalov <alexbtlv@gmail.com>
2014-07-25 15:55:42 +0600

    fix tests

--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -4,10 +4,9 @@ describe "Logging In" do
   it "logs the user in and goes to the todo lists" do
     User.create(first_name: "Jason", last_name: "Seifer", email: "jason@teamtreehouse.com", password: "treehouse1", password_confirmation: "treehouse1")
     visit "/"
-    click_link "Log In"
     fill_in "Email Address", with: "jason@teamtreehouse.com"
     fill_in "Password", with: "treehouse1"
-    click_button "Log In"
+    click_button "Sign In"
     
     expect(page).to have_content("Todo lists")
     expect(page).to have_content("Thanks for logging in!")
@@ -17,7 +16,7 @@ describe "Logging In" do
     visit new_user_session_path
     fill_in "Email Address", with: "jason@teamtreehouse.com"
     fill_in "Password", with: "incorrect"
-    click_button "Log In"
+    click_button "Sign In"
 
     expect(page).to have_content("Please check your email and password")
     expect(page).to have_field("Email Address", with: "jason@teamtreehouse.com")
