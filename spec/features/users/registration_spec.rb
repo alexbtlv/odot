7578e67901cd9938c6db4f970b0baa94febdb56c
Alexander Batalov <alexbtlv@gmail.com>
2014-07-30 18:52:12 +0600

    test user registration for tutorial

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -17,5 +17,21 @@ describe "Signing up" do
 
     expect(User.count).to eq(1)
   end
-  
+
+   it "displays a tutorial when the user signs up" do
+       visit "/"
+       within("#footr") { click_link "Sign Up" }
+
+       fill_in "First Name", with: "Jason"
+       fill_in "Last Name", with: "Seifer"
+       fill_in "Email", with: "jason@teamtreehouse.com"
+       fill_in "Password", with: "treehouse1234"
+       fill_in "Password (again)", with: "treehouse1234"
+       click_button "Sign Up"
+
+       expect(page).to have_content("Odot tutorial")
+       click_on "Odot tutorial"
+
+       expect(page.all("li.todo-item").size).to eq(7)
+    end
 end
